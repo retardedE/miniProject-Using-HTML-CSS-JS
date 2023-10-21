e5619c9ef053076ed65e9d2d925d6ce031b1050f
Emad <empsychoxlive@yahoo.com>
2023-10-21 11:21:38 -0700

    final

--- script.js
+++ script.js
@@ -18,3 +18,57 @@ for(let i=0;i<likesArray.length;i++){
         likesArray[i].style.color='red'
     })
 }
+
+
+const commentsSection=document.querySelectorAll('.commentsSection')
+const closeComments=document.querySelectorAll('#closeComments')
+const openComments=document.querySelectorAll('#openComments')
+let commentsArray= Array.from(openComments)
+let commentsSections= Array.from(commentsSection)
+let closesComments= Array.from(closeComments)
+
+
+
+
+
+for(let i=0; i<commentsArray.length;i++){
+    commentsArray[i].addEventListener('click',()=>{
+        for(let i=0;i<commentsSections.length;i++){
+            commentsSections[i].classList.remove('nonActiveComments')
+            document.body.style.overflow='hidden'
+        }
+    })
+}
+for(let i=0; i<closesComments.length;i++){
+    closesComments[i].addEventListener('click' , ()=>{
+        for(let i=0;i<commentsSections.length;i++){
+            commentsSections[i].classList.add('nonActiveComments')
+            document.body.style.overflow='auto'
+        }
+    })
+}
+
+const openShare=document.querySelector('#openShare')
+const shareSection=document.querySelector('.shareSection')
+const CloseShare=document.querySelector('#CloseShare')
+const selectShare=document.querySelectorAll('#selectShare')
+let isSelected=false
+openShare.addEventListener('click', ()=>{
+    shareSection.classList.remove('nonActiveShare')
+    document.body.style.overflow='hidden'
+})
+CloseShare.addEventListener('click', ()=>{
+    shareSection.classList.add('nonActiveShare')
+    document.body.style.overflow='auto'
+})
+for(let i=0; i<selectShare.length;i++){
+selectShare[i].addEventListener('click', ()=>{
+    if(isSelected==false){
+    selectShare[i].style.color='blue'
+    isSelected==true   
+    }else{
+        selectShare[i].style.color='black'
+        isSelected==false
+    }
+})
+}
